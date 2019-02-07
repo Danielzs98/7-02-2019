cab950a42c8ea4e68f16ea038895f1e0325d78ab
Danielzs98 <danizs@live.com.mx>
2019-02-07 09:19:25 -0600

    Create reto_2.js

---
+++ reto_2.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+
+function draw() {
+  background(0);
+	stroke(230);
+	fill(255,0,0);
+	rect(0,0,133.33,400);
+	fill(0,255,0);
+	rect(133.33,0,133.33,400);
+	fill(0,0,255);
+	rect(267.33,0,133.33,400);
+}
