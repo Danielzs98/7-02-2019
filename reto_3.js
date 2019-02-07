e49b0f6be1b5aa7ea316643df642b6ca39696a34
Danielzs98 <danizs@live.com.mx>
2019-02-07 09:32:01 -0600

    Create reto_3.js

---
+++ reto_3.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+
+function draw() {
+  background(0);
+	strokeWeight(20);
+	fill(255);
+	stroke(255,0,0);
+	ellipse(200,200,250,250);
+	line(100,140,300,275);
+	}
